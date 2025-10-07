51ee969719d7d095156a8af91e1f2210e177eab6
FREDERICK BULLEN <26595559@edgehill.ac.uk>
2025-10-07 15:47:11 +0100

    feat: complete week 1 lab exercises

---
+++ week1_lab(3).py
@@ -0,0 +1,11 @@
+print("hello world")
+studet_name = "Alex"
+student_age = 20
+print("studet name:", studet_name)
+print("student age:", student_age)
+
+#simple arithmetic 
+num1 = 15
+num2 = 10
+sum = num1 + num2
+print("the sum is:", sum)
