134093298ba71742e6a327839af71670d7ec70ed
Lezzlie <freddybullen@icloud.com>
2025-10-14 15:30:41 +0100

    "Week2_lab"

---
+++ Week2_lab.py
@@ -0,0 +1,8 @@
+"""
+This program calculates the area of a rectangle and prints the results
+"""
+temp_celsius = int(input(f"please enter teperature in celseus: ")) #this represents the temporature in degrees celsius
+temp_Fahrenheit = ((temp_celsius * 9/5)+32) # this line converts the temperature from celsius to fahrenheit
+print(f"{temp_celsius} degrees celsius is equal to {temp_Fahrenheit} degrees fahrenheit")
+
+
