31e215c88f1f99eac8d8c1c5b2704a8a82cdc103
Lezzlie <freddybullen@icloud.com>
2025-10-21 14:36:53 +0100

    I have defined a list called cost and defined a viarable total_cost, and crated a for loop which iterates through that list and desplays the cost and creates a total sum of the costs

--- week3_lab.py
+++ week3_lab.py
@@ -1,7 +1,12 @@
-count = 0
+cost =[15.00, 12.50 , 3.75, 40.25, 6.99]
+total_cost = 0
 for counter in range(0, 21, 2): # this is a for loop that will loop from 1 to 10
-    
     print(counter)   
+for price in cost :
+    print (price)
+    total_cost= total_cost + price
+print(f"Total cost is: {total_cost}")
+
 
 
     
