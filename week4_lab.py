cea746c0b99fd19f10a8ddf2636358b90ac67eb6
Lezzlie <freddybullen@icloud.com>
2025-10-28 14:23:54 +0000

    I have added a function celsius_to_farenheit which converts the value in variable c_temp its opposing farenheit value and I added a line which outputs that conversion

--- week4_lab.py
+++ week4_lab.py
@@ -1,3 +1,9 @@
+def celsius_to_farenheit(celsius): #function to convert celsius to fahrenheit
+    fahrenheit = (celsius * 9/5) + 32 #calculate fahrenheit using the celsius value
+    return fahrenheit
+
+
+
 secret_number = 245
 while True:
     Guess = int(input("guess the number"))
@@ -8,4 +14,6 @@ while True:
         print("too low, try again")
     else:
         print("too high, try again")
-
+c_temp =25
+f_temp = celsius_to_farenheit(c_temp)
+print(f"{c_temp}C is equal to {f_temp}F ")
